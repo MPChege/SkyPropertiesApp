e83cf8cbb168205c707e9d757996af59814b865c
MarkChege <mark.wanyoike@strathmore.edu>
2023-07-05 23:07:27 +0300

    fixing the errors

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from 'react';
 import './App.css';
-
-import LandingHome from './components/LandingHome'; 
-import LandingAbout from './components/LandingAbout';
-import LandingNavbar from './components/LandingNavbar';
-// import { Route } from 'react-router-dom';
-function App() {
-  return(
-    <div className='App'>
-       <LandingHome />
-       <LandingAbout />
-
-    </div>
-  )
-  
-  
-
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
 
+import LandingHome from './components/LandingHome'; 
+import LandingAbout from './components/LandingAbout';
+
 function App() {
   return (
-    <>
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/signup" element={<SignUpPage />} />
+        <Route  path="/home" element={<LandingHome />} />
+        <Route path="/about" element={<LandingAbout />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-      
     </Router>
-    </>
   );
-
 }
 
 export default App;
--- src/components/LandingHome.js
+++ src/components/LandingHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useNavigate } from 'react-router-dom';
 const BackgroundImage = styled.div`
   background-image: url("https://images.unsplash.com/photo-1635108199650-8115b597e283?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzc0fHxyZWFsJTIwZXN0YXRlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60");
   background-size: cover;
