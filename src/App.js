b6108b678a6421eac4f10132ed24f217e4ff9c3a
lewisky <lewisky28mawira@gmail.com>
2023-07-06 11:54:24 +0300

    Merge

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import React from "react";
 import "./App.css";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SignUpPage from './components/SignUpPage';
-import LoginPage from './components/LoginPage';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import LandingHome from './components/LandingHome'; 
-import LandingAbout from './components/LandingAbout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
+import { useEffect } from "react";
+import { useState } from "react";
+import LandingHome from "./components/LandingHome";
+import LandingAbout from "./components/LandingAbout";
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   // auto-login
+  //   fetch("/me").then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((user) => setUser(user));
+  //     }
+  //   });
+  // }, []);
 
-  if (!user) return <LoginPage setUser={setUser} />;
+  // if (!user) return <LoginPage setUser={setUser} />;
 
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/signup" element={<SignUpPage setUser={setUser}/>} />
+        <Route
+          exact
+          path="/signup"
+          element={<SignUpPage setUser={setUser} />}
+        />
         <Route path="/login" element={<LoginPage setUser={setUser} />} />
-        <Route  path="/" element={<LandingHome />} />
+        <Route path="/" element={<LandingHome />} />
         <Route path="/about" element={<LandingAbout />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
