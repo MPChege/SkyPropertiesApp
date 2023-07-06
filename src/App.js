da582eb6eeeaaee92e7d2fe5eb98a68f379f2581
lewisky <lewisky28mawira@gmail.com>
2023-07-06 11:08:22 +0300

    updating Landing home

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
 
-import LandingHome from './components/LandingHome'; 
-import LandingAbout from './components/LandingAbout';
-import LandingNavbar from './components/LandingNavbar';
-// import { Route } from 'react-router-dom';
-function App() {
-  return(
-    <div className='App'>
-       <LandingHome />
-       <LandingAbout />
-
-    </div>
-  )
-  
-  
-
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import SignUpPage from './components/SignUpPage';
-import LoginPage from './components/LoginPage';
+import LandingHome from "./components/LandingHome";
 
 function App() {
   return (
-    <>
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/signup" element={<SignUpPage />} />
+        <Route exact path="/" element={<LandingHome />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-      
     </Router>
-    </>
   );
-
 }
 
 export default App;
--- src/components/LandingHome.js
+++ src/components/LandingHome.js
@@ -2,34 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 const BackgroundImage = styled.div`
-  background-image: url("https://images.unsplash.com/photo-1635108199650-8115b597e283?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzc0fHxyZWFsJTIwZXN0YXRlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60");
-  background-size: cover;
-  background-position: center;
+  position: relative;
   height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+`;
+
+const ImageOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const ContentWrapper = styled.div`
-  text-align: centre;
+  text-align: center;
   color: white;
 `;
 
 const Title = styled.h1`
   font-size: 32px;
+  margin-bottom: 20px;
 `;
 
 const Subtitle = styled.p`
   font-size: 18px;
-`;
-
-const ButtonWrapper = styled.div`
   margin-bottom: 20px;
-`;
-
-const Button = styled.button`
-  margin-right: 10px;
 `;
 
 const Quote = styled.p`
@@ -39,14 +42,21 @@ const Quote = styled.p`
 const LandingHome = () => {
   return (
     <BackgroundImage>
-      <ContentWrapper>
-        <Title> SKYPROPERTIES</Title>
-        <Subtitle>
-          Welcome to our House Dealership. Find your dream home today
-        </Subtitle>
-        <Quote>"Home is where your story begins."</Quote>
-      </ContentWrapper>
+      <img
+        src="https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80"
+        alt="Background"
+      />
+      <ImageOverlay>
+        <ContentWrapper>
+          <Title>SKYPROPERTIES</Title>
+          <Subtitle>
+            Welcome to our House Dealership. Find your dream home today.
+          </Subtitle>
+          <Quote>"Home is where your story begins."</Quote>
+        </ContentWrapper>
+      </ImageOverlay>
     </BackgroundImage>
   );
 };
+
 export default LandingHome;
