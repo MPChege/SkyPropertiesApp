a5fd77aef7b9b9a085a9b0ccc10432298a09d284
sammun1926 <munorusam10@gmail.com>
2023-07-06 12:23:56 +0300

    Sellers home page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import React from "react";
 import './App.css';
-
-import Topbar from "./components/topbar/Topbar";
-import Contact from "./components/contact/contact";
-import Form from "./components/form/form";
-import Profile from "./components/profile/profile";
-import Search from "./components/search/search";
-import Houses from "./components/houses/houses";
-
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SignUpPage from './components/SignUpPage';
+import LoginPage from './components/LoginPage';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import LandingHome from './components/LandingHome'; 
+import LandingAbout from './components/LandingAbout';
+import { SellersHomePage } from './components/SellersHomePage';
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <LoginPage setUser={setUser} />;
+
   return (
-    <div className="app">
-      <Topbar />
-      <div className="sections">
-        <div className="section">
-          <Form />
-        </div>
-        <div className="section">
-          <Houses />
-        </div>
-        <div className="section">
-          <Search />
-        </div>
-        <div className="section">
-          <Profile />
-        </div>
-        <div className="section">
-          <Contact />
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/signup" element={<SignUpPage setUser={setUser}/>} />
+        <Route path="/login" element={<LoginPage setUser={setUser} />} />
+        <Route  path="/home" element={<LandingHome />} />
+        <Route path="/about" element={<LandingAbout />} />
+        <Route path='seller'  element={<SellersHomePage />}/>
+      </Routes>
+    </Router>
   );
+
 }
+
 
 export default App;
---
+++ src/components/SellersHomePage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import React from "react";
+import './App.css';
+
+import Topbar from "./components/topbar/Topbar";
+import Contact from "./components/contact/contact";
+import Form from "./components/form/form";
+import Profile from "./components/profile/profile";
+import Search from "./components/search/search";
+import Houses from "./components/houses/houses";
+
+
+export const SellersHomePage = () => {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <div className="section">
+          <Form />
+        </div>
+        <div className="section">
+          <Houses />
+        </div>
+        <div className="section">
+          <Search />
+        </div>
+        <div className="section">
+          <Profile />
+        </div>
+        <div className="section">
+          <Contact />
+        </div>
+      </div>
+    </div>
+  )
+}
