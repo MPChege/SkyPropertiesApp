0146939429b6bbb3b53cf104e383b89a15614737
sammun1926 <munorusam10@gmail.com>
2023-07-06 12:04:04 +0300

    Changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import Topbar from "./components/topbar/topbar";
+import React from "react";
+import './App.css';
+
+import Topbar from "./components/topbar/Topbar";
 import Contact from "./components/contact/contact";
 import Form from "./components/form/form";
 import Profile from "./components/profile/profile";
 import Search from "./components/search/search";
 import Houses from "./components/houses/houses";
 
-
-import './App.css';
-
 function App() {
   return (
-
     <div className="app">
-
-      <Topbar/>
-      
-   <div className="sections">
-      <Contact/>
-      <Form/>
-      <Houses/>
-      <Search/>
-      <Profile/>
+      <Topbar />
+      <div className="sections">
+        <div className="section">
+          <Form />
+        </div>
+        <div className="section">
+          <Houses />
+        </div>
+        <div className="section">
+          <Search />
+        </div>
+        <div className="section">
+          <Profile />
+        </div>
+        <div className="section">
+          <Contact />
+        </div>
       </div>
-   
     </div>
-    
-
-    
   );
 }
 
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,11 +1,19 @@
-import "./profile.css"
+import React from 'react';
+import './profile.css';
 
-export default function profile() {
+export default function Profile() {
+  const agent = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '123-456-7890',
+  };
+
   return (
     <div className="profile">
-        
-     
-        
-        </div>
-  )
+      <h2>Agent Profile</h2>
+      <p>Name: {agent.name}</p>
+      <p>Email: {agent.email}</p>
+      <p>Phone: {agent.phone}</p>
+    </div>
+  );
 }
